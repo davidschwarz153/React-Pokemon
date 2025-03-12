519e456c2bf5b0f0f6ca7119c4b7090621b9978f
Ruslan-S-Code <ruslan_cross@ua.fm>
2025-03-12 15:12:29 +0100

    Black&White Theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,34 @@ import './index.css'
 import Home from './pages/Home'
 import Layout from './layout/Layout'
 import PokemonCard from './pages/pokemonCard/PokemonCard'
+import { useState } from 'react'
+import ThemeToggleButton from './components/ThemeToggleButton'
+
 
 
 export default function App() {
+  
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
 
-const router = createBrowserRouter(createRoutesFromElements(
-    <Route path="/" element={<Layout/>}>
-      <Route index element={<Home/>} />
-      <Route path="/:name" element={<PokemonCard />} />
-    </Route>
-))
+  const toggleTheme = () => {
+    setIsDarkTheme((prevTheme) => !prevTheme);
+    document.body.classList.toggle("dark-theme", !isDarkTheme);
+    
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/:name" element={<PokemonCard />} />
+      </Route>
+    )
+  );
 
   return (
     <>
-      <RouterProvider router={router}/>
+      <ThemeToggleButton toggleTheme={toggleTheme} />
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
-
-
---
+++ src/components/ThemeToggleButton.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./ThemeToggleButton.css";
+import modeIcon from "../assets/img/mode.png";
+
+interface ThemeToggleButtonProps {
+  toggleTheme: () => void;
+}
+
+const ThemeToggleButton: React.FC<ThemeToggleButtonProps> = ({
+  toggleTheme,
+}) => {
+  return (
+    <button onClick={toggleTheme} className="icon-button">
+      <img src={modeIcon} alt="Toggle Theme" /> +
+    </button>
+  );
+};
+
+export default ThemeToggleButton;
