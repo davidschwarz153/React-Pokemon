02b41e4839de0561a016f33b25f64a0e9faf0f08
davidschwarz153 <schwarz.david100@gmail.com>
2025-03-12 14:23:26 +0100

    header added to layout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@
 
 export default function Header() {
   return (
-    <div className="pt-10">
+    <div className="pt-10 flex justify-center">
         <img src="/logo.png" alt="" />
     </div>
   )
