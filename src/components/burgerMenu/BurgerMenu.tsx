ef33d5ee62eea7411f88dd8ad774cbf2a249318b
davidschwarz153 <schwarz.david100@gmail.com>
2025-03-12 15:01:07 +0100

    fixxed burg menu

--- src/components/burgerMenu/BurgerMenu.tsx
+++ src/components/burgerMenu/BurgerMenu.tsx
@@ -1,29 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 export default function BurgerMenu() {
-    const burgerMenu: React.FC = () =>{
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-        const [isOpen, SetIsOpen] = useState<boolean>(false);
+  const toggleMenu = (): void => {
+    setIsOpen(!isOpen);
+  };
 
-        const toggleMenu = (): void =>{
-            SetIsOpen(!isOpen);
-        };
+  return (
+    <div className='relative'>
+      <button
+        onClick={toggleMenu}
+        className='p-2 rounded-md border focus:outline-none focus:ring focus:ring-orange-400'
+      >
+        <div className='space-y-1'>
+          <div className='w-6 h-0.5 bg-black' />
+          <div className='w-6 h-0.5 bg-black' />
+          <div className='w-6 h-0.5 bg-black' />
+        </div>
+      </button>
 
-        return (
-            <div className='relativ'>
-                <button onClick={toggleMenu} className='p-2 rounded-md border focus:outline-none focus:ring focus:ring-orange-400' > 
-                    <div>
-
-                    <div className='w-6 h-0.5 bg-black'> </div>
-                    <div className='w-6 h-0.5 bg-black'></div>
-                    <div className='w-6 h-0.5 bg-black'></div>
-                    </div>
-                    
-                </button>
-
+      {isOpen && (
+        <div className='absolute right-0 mt-2 w-48 bg-white border rounded-lg shadow-lg p-4 z-10'>
+          <ul className='space-y-2'>
+            <li className='hover:text-orange-500 cursor-pointer'>Home</li>
+            <li className='hover:text-orange-500 cursor-pointer'>About</li>
+            <li className='hover:text-orange-500 cursor-pointer'>Contact</li>
+          </ul>
+        </div>
+      )}
     </div>
-  )
-
-
-}
+  );
 }
