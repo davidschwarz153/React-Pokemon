1110553292f5e6326ec9f31b731e703bcb9879ec
davidschwarz153 <schwarz.david100@gmail.com>
2025-03-13 18:38:12 +0100

    deleted unused

--- src/components/burgerMenu/BurgerMenu.tsx
+++ src/components/burgerMenu/BurgerMenu.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+
 import "./BurgerMenu.css";
 import { Link } from "react-router-dom";
 
 export default function BurgerMenu() {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+ 
 
 
 
