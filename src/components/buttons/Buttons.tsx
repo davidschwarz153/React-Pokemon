9ec66552a238a1dd327ac9490e363a12654463f9
Aylin <77777eileen@gmail.com>
2025-03-13 10:38:49 +0100

    update

---
+++ src/components/buttons/Buttons.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import { typeColors } from '../typeColors/typeColors'
+
+
+
+export default function Buttons() {
+
+interface ColorSearchProps {
+    onColorSelect: (color:string)=> void;
+}
+const ColorSearch: React.FC<ColorSearchProps> =({ onColorSelect}) =>{
+    const colors: Color[] = []
+}
+
+// const [color, setColor] = useState<string[]>()
+
+// btnClr == pkmClr
+
+  return (
+<div className='flex '>
+{.map((color))}
+
+
+</div>
+
+
+
+
+    // <div>
+    //     <h2>TYPE</h2>
+    //   <button>BUG</button>
+    //   <button>DARK</button>
+    //   <button>DRAGON</button>
+    //   <button>ELECTRIC</button>
+    //   <button>FAIRY</button>
+    //   <button>FIGHTING</button>
+    //   <button>FIRE</button>
+    //   <button>FLYING</button>
+    //   <button>GHOST</button>
+    //   <button>GRASS</button>
+    //   <button>GROUND</button>
+    //   <button>ICE</button>
+    //   <button>NORMAL</button>
+    //   <button>PLANET</button>
+    //   <button>POISON</button>
+    //   <button>PSYCHIC</button>
+    //   <button>ROCK</button>
+    //   <button>STEEL</button>
+    //   <button>WATER</button>
+    // </div>
+  )
+}
